186f81df02a4b2801036a64ea1ad01a2d6736113
bhaskar.p <bhaskar.p@PENNANT-VSP185.pennant.local>
2023-06-15 16:21:10 +0530

    first commit

---
+++ Induction/src/models/Induction.java
@@ -0,0 +1,99 @@
+package models;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "hrms_induction")
+public class Induction {
+	@Id
+	@Column(name = "indc_id")
+	private int indcId;
+
+	@Column(name = "indc_emof_id")
+	private int indcEmofId;
+
+	@Column(name = "indc_date")
+	private Date indcDate;
+
+	@Column(name = "indc_processes_ausr_id")
+	private int indcProcessedAusrId;
+
+	@Column(name = "indc_status")
+	private String indcStatus;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "indc_emof_id", referencedColumnName = "eofr_cand_id", insertable = false, updatable = false)
+	private HrmsEmploymentOffer employmentOffer;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "indc_processes_ausr_id", referencedColumnName = "ausr_empl_id", insertable = false, updatable = false)
+	private HrmsAdminUser adminUser;
+
+	public int getIndcId() {
+		return indcId;
+	}
+
+	public void setIndcId(int indcId) {
+		this.indcId = indcId;
+	}
+
+	public int getIndcEmofId() {
+		return indcEmofId;
+	}
+
+	public void setIndcEmofId(int indcEmofId) {
+		this.indcEmofId = indcEmofId;
+	}
+
+	public Date getIndcDate() {
+		return indcDate;
+	}
+
+	public void setIndcDate(Date indcDate) {
+		this.indcDate = indcDate;
+	}
+
+	public int getIndcProcessedAusrId() {
+		return indcProcessedAusrId;
+	}
+
+	public void setIndcProcessedAusrId(int indcProcessedAusrId) {
+		this.indcProcessedAusrId = indcProcessedAusrId;
+	}
+
+	public String getIndcStatus() {
+		return indcStatus;
+	}
+
+	public void setIndcStatus(String indcStatus) {
+		this.indcStatus = indcStatus;
+	}
+
+	public HrmsEmploymentOffer getEmploymentOffer() {
+		return employmentOffer;
+	}
+
+	public void setEmploymentOffer(HrmsEmploymentOffer employmentOffer) {
+		this.employmentOffer = employmentOffer;
+	}
+
+	public HrmsAdminUser getAdminUser() {
+		return adminUser;
+	}
+
+	public void setAdminUser(HrmsAdminUser adminUser) {
+		this.adminUser = adminUser;
+	}
+
+	// Constructors, getters, and setters
+	// ...
+
+}
